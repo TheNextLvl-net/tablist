a4cca8f997147bb641e48446725404d1606854f9
NonSwag <54660361+NonSwag@users.noreply.github.com>
2024-07-16 11:36:54 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,12 +20,12 @@ repositories {
 dependencies {
     compileOnly("net.luckperms:api:5.4")
     compileOnly("org.projectlombok:lombok:1.18.28")
-    compileOnly("com.velocitypowered:velocity-api:3.2.0-SNAPSHOT")
+    compileOnly("com.velocitypowered:velocity-api:3.3.0-SNAPSHOT")
 
     implementation("net.thenextlvl.core:files:1.0.5")
 
     annotationProcessor("org.projectlombok:lombok:1.18.28")
-    annotationProcessor("com.velocitypowered:velocity-api:3.2.0-SNAPSHOT")
+    annotationProcessor("com.velocitypowered:velocity-api:3.3.0-SNAPSHOT")
 }
 
 tasks.shadowJar {
