f10f8d92b49ddc08a1a7ec0489a77e30b566ca3e
david <mrminecraft00@gmail.com>
2024-11-20 16:08:54 +0100

    [ci skip] Update velocity api to 3.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,12 +20,12 @@ repositories {
 dependencies {
     compileOnly("net.luckperms:api:5.4")
     compileOnly("org.projectlombok:lombok:1.18.36")
-    compileOnly("com.velocitypowered:velocity-api:3.3.0-SNAPSHOT")
+    compileOnly("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
 
     implementation("net.thenextlvl.core:files:2.0.0")
 
     annotationProcessor("org.projectlombok:lombok:1.18.36")
-    annotationProcessor("com.velocitypowered:velocity-api:3.3.0-SNAPSHOT")
+    annotationProcessor("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
 }
 
 tasks.shadowJar {
